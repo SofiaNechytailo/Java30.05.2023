64a2385e10a5e39bb1fc83a42ad9ba12736afeab
SofiaNechytailo <134634542+SofiaNechytailo@users.noreply.github.com>
2023-06-02 22:13:55 +0200

    update with scanner

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-       int a=354;
-        int b=5478890;
-      System.out.println(a+b);
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        int a = in.nextInt();
+        Scanner inb = new Scanner(System.in);
+        System.out.print("Input b number: ");
+        int b = inb.nextInt();
+        Scanner inc = new Scanner(System.in);
+        System.out.print("Input c number: ");
+        int c = inc.nextInt();
+        in.close();
+        inb.close();
+        inc.close();
+
+
+        System.out.println(a+b);
         System.out.println(a-b);
         System.out.println(a*b);
         System.out.println(a/b);
+        System.out.println(c);
     }
 }
